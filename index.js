1e41ef2764c768ed84a02f473ff05cd520b99710
RD1707 <souplemonpie@gmail.com>
2025-08-06 13:46:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -16,20 +16,23 @@ app.post('/calcular-imc', (req, res) => {
     const imc = peso / (altura * altura);
 
     let categoria = '';
-    if (imc < 18.5) { 
-        categoria = 'Baixo peso';
-    } else if (imc < 25) {
+    if (imc < 16) {
+        categoria = 'Baixo peso (grau I)';
+    } else if (imc >= 16 && imc <= 16.99) {
+        categoria = 'Baixo peso (grau II)';
+    } else if (imc >= 17 && imc <= 18.49) {
+        categoria = 'Baixo peso (grau III)';
+    } else if (imc >= 18.5 && imc <= 24.99) {
         categoria = 'Peso adequado';
-    } else if (imc < 30) {
+    } else if (imc >= 25 && imc <= 29.99) {
         categoria = 'Sobrepeso';
-    } else if (imc < 35) {
+    } else if (imc >= 30 && imc <= 34.99) {
         categoria = 'Obesidade (grau I)';
-    } else if (imc < 40) {
+    } else if (imc >= 35 && imc <= 39.99) {
         categoria = 'Obesidade (grau II)';
     } else {
         categoria = 'Obesidade (grau III)';
     }
-
     res.send(`
         <h1>Olá, ${nome}!</h1>
         <p>Seu IMC é <strong>${imc.toFixed(2)}</strong>.</p>
